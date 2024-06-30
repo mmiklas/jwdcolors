2aba1be1786e459f5a03f36b7df3d7faaac29db4
Marcin M. <m.miklas@wszib.edu.pl>
2024-06-30 10:15:51 +0200

    Konfiguracja bazy danych

--- src/main/java/pl/edu/wszib/jwd/jwdcolors/JwdcolorsApplication.java
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/JwdcolorsApplication.java
@@ -2,7 +2,9 @@ package pl.edu.wszib.jwd.jwdcolors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:db.properties")
 @SpringBootApplication
 public class JwdcolorsApplication {
 
